6cff69114c27adf42accb2196d8eaf933d70b83f
idothings.my <idothings.my@gmail.com>
2025-12-03 04:38:33 +0000

    feat: add admob configuration

--- app.config.js
+++ app.config.js
@@ -42,7 +42,8 @@ module.exports = {
       [
         "react-native-google-mobile-ads",
         {
-          "android_app_id": "ca-app-pub-7556071990692700~5301219719"
+          "android_app_id": "ca-app-pub-7556071990692700~5301219719",
+          "ios_app_id": "ca-app-pub-7556071990692700~3419956649"
         }
       ]
     ],
